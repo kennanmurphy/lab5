c936692924913cad9a3730a11b5a65d1f87c3c37
vagrant <vagrant@precise32.(none)>
2014-02-06 06:57:50 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,11 +10,19 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".anName").click(listenerFunction);
+}
+
+function listenerFunction(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	var newText = anagrammedName(name);
+	$(this).text(newText);
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
 	} 
